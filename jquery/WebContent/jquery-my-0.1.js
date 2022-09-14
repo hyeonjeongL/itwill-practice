cd9aa2cacdbcf4c9b2445761f79d346fc5ad851a
hyeonjeongL <hunnnn94@gmail.com>
2022-09-14 17:54:24 +0900

    ..

--- jquery/WebContent/jquery-my-0.1.js
+++ jquery/WebContent/jquery-my-0.1.js
@@ -1,49 +1,91 @@
 window.jQuery = function(arg) {
-	
-	
+
+
 	if (typeof arg == 'string') {
 		var elementNodeList = document.querySelectorAll(arg);
-		
-		var jqueryWrapperObject={
-			'elementNodeList':elementNodeList,
-			'css':function(propertyName,propertyValue){
-				for(var i=0;i<this.elementNodeList.length;i++){
+
+		var jqueryWrapperObject = {
+			'elementNodeList': elementNodeList,
+			'css': function(propertyName, propertyValue) {
+				for (var i = 0; i < this.elementNodeList.length; i++) {
 					this.elementNodeList[i].style.cssText += `${propertyName}:${propertyValue};`;
 				}
 				return this;
-				
+
 			},
-			'text':function(textArg){
-				for(var i=0; i<this.elementNodeList.length;i++){
-					//this.elementNodeList[i].inner=textArg; 똑같진 않으나 사용가능
-					this.elementNodeList[i].firstChild.nodeValue=textArg;
+			'text': function(textArg) {
+				if (textArg == undefined) {
+					//get text
+					var returnText = "";
+					for (var i = 0; i < this.elementNodeList.length; i++) {
+						returnText += this.elementNodeList[i].firstChild.nodeValue;
+					}
+					return returnText;
+				} else {
+					//set text
+					for (var i = 0; i < this.elementNodeList.length; i++) {
+						//this.elementNodeList[i].inner=textArg; 똑같진 않으나 사용가능
+						this.elementNodeList[i].firstChild.nodeValue = textArg;
+					}
 				}
 				return this;
 			}
 		};
 		return jqueryWrapperObject;
-		
-		
-		
-	}else if(typeof arg =='function'){
+
+
+
+	} else if (typeof arg == 'function') {
 		/*
 		인자로 대입된 함수를 DOM트리생성직후에 호출되도록
 		window.onload이벤트에 등록
 		*/
-	}else if(typeof arg =='object'){
+		window.addEventListener('load', arg);
+	} else if (typeof arg == 'object') {
 		/*
 		표준객체(Element,document,window,...)
 		*/
+		var elementNodeList = new Array();
+		elementNodeList.push(arg);
+
+		var jQueryWrapperObject = {
+			'elementNodeList': elementNodeList,
+			'css': function(propertyName, propertyValue) {
+				for (var i = 0; i < this.elementNodeList.length; i++) {
+					this.elementNodeList[i].style.cssText += `${propertyName}:${propertyValue};`;
+				}
+				return this;
+
+			},
+			'text': function(textArg) {
+				if (textArg == undefined) {
+					//get text
+					var returnText = "";
+					for (var i = 0; i < this.elementNodeList.length; i++) {
+						returnText += this.elementNodeList[i].firstChild.nodeValue;
+					}
+					return returnText;
+				} else {
+					//set text
+					for (var i = 0; i < this.elementNodeList.length; i++) {
+						//this.elementNodeList[i].inner=textArg; 똑같진 않으나 사용가능
+						this.elementNodeList[i].firstChild.nodeValue = textArg;
+					}
+				}
+				return this;
+			}
+		};
+		return jQueryWrapperObject;
 	}
-	
+
 };
 
 /*******jQuery global function*************/
-window.jQuery.each=function(array,funcArg){
-	for(var i=0;i<array.length;i++){
-	funcArg(i,array[i]);
-		
+window.jQuery.each = function(array, funcArg) {
+	for (var i = 0; i < array.length; i++) {
+		funcArg(i, array[i]);
+
 	}
 };
 
-window.$=window.jQuery;
+window.$ = window.jQuery;
