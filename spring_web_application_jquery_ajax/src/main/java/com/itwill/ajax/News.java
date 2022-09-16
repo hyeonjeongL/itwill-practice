a5bc26475fd82e4dfe3ba15923e94fd5aa29f834
hyeonjeongL <hunnnn94@gmail.com>
2022-09-16 18:06:08 +0900

    ..

---
+++ spring_web_application_jquery_ajax/src/main/java/com/itwill/ajax/News.java
@@ -0,0 +1,49 @@
+package com.itwill.ajax;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
+
+
+@JacksonXmlRootElement(localName="news")
+public class News {
+	
+	private String title;
+	private String company;
+	private String date;
+	public News() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public News(String title, String company, String date) {
+		super();
+		this.title = title;
+		this.company = company;
+		this.date = date;
+	}
+	@JsonProperty(value = "newsTitle")
+	@JacksonXmlProperty(localName = "newsTitle")
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
---
+++ spring_web_application_jquery_ajax/src/main/java/com/itwill/ajax/NewsListResult.java
@@ -0,0 +1,28 @@
+package com.itwill.ajax;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+public class NewsListResult {
+	private int count;
+	private List<News> newsList;
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public List<News> getNewsList() {
+		return newsList;
+	}
+
+	public void setNewsList(List<News> newsList) {
+		this.newsList = newsList;
+	}
+
+}
